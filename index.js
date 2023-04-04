62c704b7db32a08bf75e73fbc1a82e4d3adbd405
Ruthmy <Ruryemo.exe@gmail.com>
2023-04-04 14:59:11 -0600

    Setted the Luxon library and the time-date on the index

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { linkList, linkAdd, linkContact } from './modules/navegation.js';
 import { addBook, removeBook, displayBooks } from './modules/crud.js';
+import { DateTime } from './modules/luxon.js';
 
-// Add date & time
-const date = new Date();
-const timeDate = document.querySelector('#date');
-timeDate.innerHTML = date.toString();
+// create a new DateTime object
+const now = DateTime.now();
+// format the date and time using Luxon's API
+const formatted = now.toLocaleString(DateTime.DATETIME_HUGE);
+// Getting the HTML element to put the formatted date
+const dateSection = document.getElementById('date');
+dateSection.textContent = formatted;
